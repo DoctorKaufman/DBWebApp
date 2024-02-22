5096334cfd937d2b77e54126e621965758d14afb
mx_lshk <max.loshak.i@gmail.com>
2024-02-23 00:27:08 +0200

    login lemplates + routing through views

--- app/views/main.py
+++ app/views/main.py
@@ -6,3 +6,8 @@ main = Blueprint('main', __name__)
 def home():
     user = {'username': 'John Doe'}  # Data to be passed to the template
     return render_template('home.html', user=user)
+
+@main.route('/login')
+def login():
+    user = {'username': 'John Doe'} 
+    return render_template('login.html', user=user)
