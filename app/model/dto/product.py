f8cbbc2c76e55a0785d47fac27016fe16e6518d6
andrey.krasovskyy@gmail.com <andrey.krasovskyy@gmail.com>
2024-03-29 22:06:31 +0200

    All DTO's complete

---
+++ app/model/dto/product.py
@@ -0,0 +1,22 @@
+class ProductDTO:
+    def __init__(self, id_product, category_number, product_name, p_characteristics):
+        self.__id_product = id_product
+        self.__category_number = category_number
+        self.__product_name = product_name
+        self.__p_characteristics = p_characteristics
+
+    @property
+    def id_product(self):
+        return self.__id_product
+
+    @property
+    def category_number(self):
+        return self.__category_number
+
+    @property
+    def product_name(self):
+        return self.__product_name
+
+    @property
+    def p_characteristics(self):
+        return self.__p_characteristics
