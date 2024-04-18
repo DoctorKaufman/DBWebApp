748f44e492db2374c9b3a7e52c1b25f0629d8f77
Danylo Shlapak <geografy2314@gmail.com>
2024-04-18 12:47:32 +0300

    added base login

---
+++ app/controllers/authentication_controller.py
@@ -0,0 +1,24 @@
+import json
+
+from flask import Blueprint, request
+
+from app.controllers.connector.db_connector import get_connection
+from app.controllers.dtos.login import LoginDTO
+from app.model.repository.employee import EmployeeRepository
+from app.model.repository.employee_account import EmployeeAccountRepository
+from app.services.auth_service import AuthService
+
+auth = Blueprint('auth', __name__, url_prefix='/auth')
+
+auth_repository = EmployeeAccountRepository(get_connection())
+employee_repository = EmployeeRepository(get_connection())
+auth_service = AuthService(auth_repository, employee_repository)
+
+
+@auth.route('/login', methods=['POST'])
+def login():
+    login_data = LoginDTO.deserialize(request.get_json())
+    authenticated = auth_service.authenticate(login_data)
+    if authenticated is not None:
+        return authenticated.serialize(), 200
+    return "None", 401
