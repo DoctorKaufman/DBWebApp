5377f2e417411f53c1562fd970c3100ec70ce2d8
andrey.krasovskyy@gmail.com <andrey.krasovskyy@gmail.com>
2024-04-26 10:36:20 +0300

    Customer query

--- app/static/js/request2.js
+++ app/static/js/request2.js
@@ -1,13 +1,17 @@
+import {sendRequest} from "./sendRequest";
 
 document.addEventListener('alpine:init', () => {
-    Alpine.data('request1',() => ({
-        result: null,
+    Alpine.data('request2',() => ({
+        result: [],
+        async init() {this.sendRequest()},
 
         async sendRequest() {
             let baseUrl = window.location.origin;
-            axios.get(`${baseUrl}/request1`)
+            //axios.get(`${baseUrl}/query/2`)
+            axios.get(`http://127.0.0.1:5000/query/2`)
                 .then(response => {
                     this.result = response;
+                    console.log(this.result);
                 })
                 .catch(error => {
                     console.error('Error fetching data:', error);
