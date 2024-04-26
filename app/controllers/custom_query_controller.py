8cf8bda9ef630381a7d6e2ff9887b954d688eb76
mx_lshk <max.loshak.i@gmail.com>
2024-04-26 17:27:40 +0300

    my working version

--- app/controllers/custom_query_controller.py
+++ app/controllers/custom_query_controller.py
@@ -2,25 +2,25 @@ import json
 
 from flask import Blueprint, request
 from app.controllers.connector.db_connector import get_connection
-from app.model.repository.category_in_stock import CategoryInStockRepository
-from app.model.repository.customer import CustomerRepository
+from app.model.repository.category_total_cost import CategoryTotalCostRepository
+from app.model.repository.employee_all_customers import EmployeeAllCustomersRepository
 from app.services.custom_query_service import CustomQueryService
 
 query = Blueprint('query', __name__, url_prefix='/query')
 
-customer_repository = CustomerRepository(get_connection())
-category_repository = CategoryInStockRepository(get_connection())
+category_repository = CategoryTotalCostRepository(get_connection())
+employee_repository = EmployeeAllCustomersRepository(get_connection())
 
-customer_service = CustomQueryService(customer_repository, category_repository)
+custom_service = CustomQueryService(category_repository, employee_repository)
 
 
 @query.route('/1', methods=['GET'])
 def get_customers():
     args = request.args
-    min_amount = float(args['value'])
-    return json.dumps([c.serialize() for c in customer_service.get_customers_statistics(min_amount)])
+    given_number = float(args['value'])
+    return json.dumps([c.serialize() for c in custom_service.get_category_total_cost(given_number)])
 
 
 @query.route('/2', methods=['GET'])
 def get_categories():
-    return json.dumps([c.serialize() for c in customer_service.get_categories_in_stock()])
+    return json.dumps([c.serialize() for c in custom_service.get_employees()])
--- app/services/custom_query_service.py
+++ app/services/custom_query_service.py
@@ -1,14 +1,15 @@
-from app.model.repository.category_in_stock import CategoryInStockRepository
-from app.model.repository.customer import CustomerRepository
+from app.model.repository.category_total_cost import CategoryTotalCostRepository
+from app.model.repository.employee_all_customers import EmployeeAllCustomersRepository
 
 
 class CustomQueryService:
-    def __init__(self, customer_repository: CustomerRepository, category_stock_repository: CategoryInStockRepository):
-        self.customer_repository = customer_repository
-        self.category_stock_repository = category_stock_repository
+    def __init__(self, category_repository: CategoryTotalCostRepository,
+                 employee_repository: EmployeeAllCustomersRepository):
+        self.category_repository = category_repository
+        self.employee_repository = employee_repository
 
-    def get_customers_statistics(self, min_amount: float):
-        return self.customer_repository.get_customers(min_amount)
+    def get_category_total_cost(self, given_number: float):
+        return self.category_repository.get_category_total_cost(given_number)
 
-    def get_categories_in_stock(self):
-        return self.category_stock_repository.get_categories()
+    def get_employees(self):
+        return self.employee_repository.get_employees()
